44e5a5093b14176070d8f6fc70fa7a59038b9a51
npetrov <petrova.nataliia007@gmail.com>
2020-12-28 10:45:24 +0200

    28.12

--- Master/Tables/Products.sql
+++ Master/Tables/Products.sql
@@ -2,8 +2,8 @@
     [ProductID]          INT            IDENTITY (1, 1) NOT NULL,
     [ProductName]        NVARCHAR (100) NULL,
     [ProductDescription] NVARCHAR (MAX) NULL,
-    [BrandID]            INT            NULL,
-    [CategoryID]         INT            NULL,
+    [BrandID]            INT            NULL CONSTRAINT FK_ProductsBrands FOREIGN KEY ([BrandID]) REFERENCES [MASTER].[Brands] ([BrandID]),
+    [CategoryID]         INT            NULL CONSTRAINT FK_ProductsCategories FOREIGN KEY ([CategoryID]) REFERENCES [MASTER].[Categories] ([CategoryID]),
     PRIMARY KEY CLUSTERED ([ProductID] ASC)
 );
 
--- dbo/Tables/TEST3.sql
+++
@@ -1,7 +0,0 @@
-﻿CREATE TABLE [dbo].[TEST3] (
-    [ID]     INT          IDENTITY (1, 1) NOT NULL,
-    [NAME]   VARCHAR (30) NOT NULL,
-    [SALARY] MONEY        NULL,
-    UNIQUE NONCLUSTERED ([ID] ASC)
-);
-
