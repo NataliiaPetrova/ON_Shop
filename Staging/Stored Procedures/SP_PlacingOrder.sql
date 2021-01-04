ca700f591d1ee9b99b83c953c5ca9730d6dbc66c
npetrov <petrova.nataliia007@gmail.com>
2021-01-04 12:29:54 +0200

    04.01

---
+++ Staging/Stored Procedures/SP_PlacingOrder.sql
@@ -0,0 +1,64 @@
+﻿--========================================================================================================--
+/*
+	   Object:				StoredProcedure [Staging].[SP_BCPNewDeliveryIN]    
+	   Script Date:			30.12.2020 16:50:55 
+	   Short Description:	Creating an actual order
+	   Scripted by:			LV4097\NATALIIAPETROVA
+*/
+--========================================================================================================--
+CREATE  PROCEDURE [Staging].[SP_PlacingOrder](
+@OrderID  INT
+)
+AS
+
+BEGIN TRY 
+
+DECLARE
+@StartOrderDetailID INT,
+@EndOrderDetailID INT,
+@CurrentOrderDetailID INT,
+@CurrentStockID INT
+
+-- Create a new Version for the current order
+EXECUTE [Staging].[SP_VersionOrder] @OrderID 
+-- SELECT 'OrderDetailID' for chosen @OrderID
+	SELECT @StartOrderDetailID = MIN(OrderDetails.OrderDetailID), 
+		   @EndOrderDetailID   = MAX(OrderDetails.OrderDetailID)
+	FROM Master.OrderDetails 
+
+
+
+	SET @CurrentOrderDetailID = @StartOrderDetailID
+
+	-- selecting a current product from the 'Stock' table and update necessary fields
+		WHILE @CurrentOrderDetailID <= @EndOrderDetailID
+			BEGIN
+				SELECT @CurrentStockID = StockID 
+					FROM Master.Stocks
+					WHERE ProductID = (SELECT ProductID 
+										FROM Master.OrderDetails
+										WHERE OrderDetailID = @CurrentOrderDetailID)
+					  AND EndVersion = 999999999
+					ORDER BY StartVersion, StockID
+					OFFSET 0 ROWS FETCH FIRST 1 ROWS ONLY
+
+					UPDATE Master.Stocks
+						SET EndVersion = IDENT_CURRENT('Master.VersionConfigs')  
+						WHERE StockID = @CurrentStockID
+
+		
+			  SET @CurrentOrderDetailID +=1 
+			END
+
+END TRY 
+
+BEGIN CATCH
+
+	DECLARE 
+	@ErrorMessage nvarchar (max) = ERROR_MESSAGE(),
+	@ErrorNumber int = ERROR_NUMBER(),
+	@ErrortProcName VARCHAR(250) = OBJECT_SCHEMA_NAME(@@PROCID)+'.'+OBJECT_NAME(@@PROCID)
+
+	EXEC [Logs].[SP_Errors]  @ErrortProcName=@ErrortProcName, @ErrorMessage = @ErrorMessage, @ErrorNumber =  @ErrorNumber 
+
+END CATCH;
---
+++ Staging/Stored Procedures/SP_VersionOrder.sql
@@ -0,0 +1,47 @@
+﻿--========================================================================================================--
+/*
+	   Object:				StoredProcedure [Staging].[SP_VersionOrder]   
+	   Script Date:			30.12.2020 16:50:55 
+	   Short Description:	Creating version for an order
+	   Scripted by:			LV4097\NATALIIAPETROVA
+*/
+--========================================================================================================--
+
+CREATE  PROCEDURE [Staging].[SP_VersionOrder](
+@OrderID INT
+)
+AS
+
+BEGIN TRY 
+
+DECLARE
+	@EventProcName VARCHAR(250) = OBJECT_SCHEMA_NAME(@@PROCID)+'.'+OBJECT_NAME(@@PROCID),
+	@RowCount INT
+
+    -- logging events
+	EXECUTE Logs.SP_EventR @EventProcName, @rowcount 
+
+INSERT INTO [Master].VersionConfigs (VersionDateTime, OperationRunID)
+	SELECT OrderDate, 
+		   IDENT_CURRENT('Logs.OperationRuns') AS OperationRunID 
+		FROM Master.Orders
+		WHERE OrderID = @OrderID
+	
+		-- Calculate and save how many rows were populeted
+		SET @RowCount = (@@ROWCOUNT)  
+
+    -- completing logging
+	EXECUTE Logs.SP_EventR @EventProcName, @rowcount 
+
+END TRY
+
+BEGIN CATCH
+
+	DECLARE 
+	@ErrorMessage nvarchar (max) = ERROR_MESSAGE(),
+	@ErrorNumber int = ERROR_NUMBER(),
+	@ErrortProcName VARCHAR(250) = OBJECT_SCHEMA_NAME(@@PROCID)+'.'+OBJECT_NAME(@@PROCID)
+
+	EXEC [Logs].[SP_Errors]  @ErrortProcName=@ErrortProcName, @ErrorMessage = @ErrorMessage, @ErrorNumber =  @ErrorNumber 
+
+END CATCH;
